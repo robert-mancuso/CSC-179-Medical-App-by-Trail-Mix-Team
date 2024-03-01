035591f0f029504afefcdfb29080ece80ef8c3c0
robert-mancuso <51728732+robert-mancuso@users.noreply.github.com>
2024-02-29 20:21:33 -0800

    Initial commit
    Generated by react-native@0.73.5

---
+++ android/app/src/main/java/com/csc_179_medical_tracking_app/MainActivity.kt
@@ -0,0 +1,22 @@
+package com.csc_179_medical_tracking_app
+
+import com.facebook.react.ReactActivity
+import com.facebook.react.ReactActivityDelegate
+import com.facebook.react.defaults.DefaultNewArchitectureEntryPoint.fabricEnabled
+import com.facebook.react.defaults.DefaultReactActivityDelegate
+
+class MainActivity : ReactActivity() {
+
+  /**
+   * Returns the name of the main component registered from JavaScript. This is used to schedule
+   * rendering of the component.
+   */
+  override fun getMainComponentName(): String = "CSC_179_Medical_Tracking_App"
+
+  /**
+   * Returns the instance of the [ReactActivityDelegate]. We use [DefaultReactActivityDelegate]
+   * which allows you to enable New Architecture with a single boolean flags [fabricEnabled]
+   */
+  override fun createReactActivityDelegate(): ReactActivityDelegate =
+      DefaultReactActivityDelegate(this, mainComponentName, fabricEnabled)
+}
