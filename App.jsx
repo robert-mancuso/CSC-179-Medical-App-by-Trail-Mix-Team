46273bbb7f30a86f7737dc09dab6d33b81884722
jazzypp <jasminep3233@gmail.com>
2024-04-16 22:49:45 -0700

    Home screen with login and register features

--- App.jsx
+++ App.jsx
@@ -1,17 +1,21 @@
-// App.jsx
 import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
-import DashboardScreen from './DashboardScreen'; // Assume this is the path to your component
-import ScheduleAppointmentScreen from './ScheduleAppointmentScreen'; // And this one too
+import DashboardScreen from './DashboardScreen';
+import ScheduleAppointmentScreen from './ScheduleAppointmentScreen';
 import PatientScreen from './PatientScreen';
-
+import LoginScreen from './Home/loginscreen'; 
+import RegisterScreen from './Home/registerscreen'; 
+import LoginRegisterScreen from './Home/login-register';
 const Stack = createNativeStackNavigator();
 
 function App() {
     return (
         <NavigationContainer>
-            <Stack.Navigator initialRouteName="Dashboard">
+            <Stack.Navigator initialRouteName="LoginRegisterScreen">
+                <Stack.Screen name="LoginRegisteScreen" component={LoginRegisterScreen} options={{ title: 'LoginRegisterScreen' }} />
+                <Stack.Screen name="LoginScreen" component={LoginScreen} options={{ title: 'LoginScreen' }} />
+                <Stack.Screen name="RegisterScreen" component={RegisterScreen} options={{ title: 'RegisterScreen' }} />
                 <Stack.Screen name="Dashboard" component={DashboardScreen} options={{ title: 'Doctor\'s Dashboard' }} />
                 <Stack.Screen name="ScheduleAppointment" component={ScheduleAppointmentScreen} options={{ title: 'Schedule Appointment' }} />
                 <Stack.Screen name="Patient List" component={PatientScreen} options={{ title: 'Patient List' }} />
---
+++ Home/loginscreen.js
@@ -0,0 +1,94 @@
+import React, { useState } from 'react';
+import { View, TextInput, Button, StyleSheet, Text, ImageBackground } from 'react-native';
+import { useLayoutEffect } from 'react';
+import { useNavigation } from '@react-navigation/native';
+
+const LoginScreen = () => {
+  const [username, setUsername] = useState(''); 
+  const [password, setPassword] = useState(''); 
+  const navigation = useNavigation(); 
+  useLayoutEffect(() => {
+    navigation.setOptions({
+      headerShown: false,
+    });
+  }, [navigation]);
+  const handleLogin = () => {
+    // login logic here
+    // check the username and password against authentication service
+    const correctUsername = 'test123';
+    const correctPassword = 'pwtest';
+    if (username === correctUsername && password === correctPassword) {
+      console.log('Login successful');
+      navigation.navigate('Dashboard');
+    } else {
+      console.log('Login failed: Incorrect username or password');    
+      // navigation.navigate('Dashboard');
+    }
+  };
+
+  return (
+    <ImageBackground
+    source={require('./images/background.png')}
+    style={styles.background}
+    >
+    <View style={styles.container}>
+      <TextInput 
+        placeholder="Username" 
+        placeholderTextColor="#000"
+        style={styles.input}
+        value={username}
+        onChangeText={setUsername} 
+      />
+      <TextInput 
+        placeholder="Password" 
+        placeholderTextColor="#000"
+        secureTextEntry 
+        style={styles.input}
+        value={password}
+        onChangeText={setPassword} 
+      />
+      <Button title="Submit" onPress={handleLogin} />
+      <Text style={styles.registerText}>
+        Don't have an account? {/* Added a space here */}
+        <Text 
+          style={styles.registerLink}
+          onPress={() => navigation.navigate('RegisterScreen')}
+        >
+        Register now!
+        </Text>
+      </Text>
+    </View>
+    </ImageBackground>
+  );
+};
+
+const styles = StyleSheet.create({
+background: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    width: '100%',
+    height: '100%',
+    resizeMode: 'cover',
+},    
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    padding: 20,
+  },
+  input: {
+    borderBottomWidth: 1,
+    marginBottom: 20,
+    padding: 10,
+  },
+  registerText: {
+    marginTop: 20,
+    textAlign: 'center',
+  },
+  registerLink: {
+    color: 'blue',
+    fontWeight: 'bold',
+  },
+});
+
+export default LoginScreen;
